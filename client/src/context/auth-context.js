aa3d6e2df54bed7d988b303ee87bb4af5e14a5a5
Nicolás Bárcena <nicolas.barcena@kazaro.com>
2026-01-19 15:01:16 -0300

    12

--- client/src/context/auth-context.js
+++ client/src/context/auth-context.js
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 /*comentario*/ 
 export const AuthContext = createContext(null);
-
+/*coment*/
 export function useAuth() {
   return useContext(AuthContext);
 }
